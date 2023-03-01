f1321eaa5ef20943d48149fa1c97b53a1bb4a81a
bingual <bingual76@gmail.com>
2023-03-01 18:56:38 +0900

    initial

--- test-react/server/routers/board.js
+++ test-react/server/routers/board.js
@@ -13,7 +13,7 @@ router.get('/posts', isAuthenticated, (req, res) => {
     const sql = 'select * from board order by id desc';
     db.query(sql, (err, data) => {
         if (!err && data) {
-            res.send(data);
+            res.json(data);
         } else res.json(err);
     });
 });
--- test-react/src/index.js
+++ test-react/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import 'antd/dist/antd.min.css';
-import Signup from 'pages/accounts/Signup';
 import { BrowserRouter } from 'react-router-dom';
 import Root from 'pages';
 
